9c1c3f13c3c2978d4afe649ace3e9c7e32271cb6
Lenin Paulino <leninpaulinon@gmail.com>
2021-05-04 04:58:59 -0400

    feat: sort articles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import './App.css';
-import 'h8k-components';
+import React, { useState } from 'react'
+import './App.css'
+import 'h8k-components'
 
-import Articles from './components/Articles';
+import Articles from './components/Articles'
 
-const title = "Sorting Articles";
+const title = 'Sorting Articles'
 
-function App({articles}) {
-
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
-    );
-
+function upvotesComparison(a, b) {
+  if (a.upvotes > b.upvotes) {
+    return -1
+  }
+  if (a.upvotes < b.upvotes) {
+    return 1
+  }
+  return 0
 }
 
-export default App;
+function sortArticlesByUpvotes(articles) {
+  return articles.sort(upvotesComparison)
+}
+
+function datesComparison(a, b) {
+  const aDate = new Date(a.date)
+  const bDate = new Date(b.date)
+  if (aDate > bDate) {
+    return -1
+  }
+  if (aDate < bDate) {
+    return 1
+  }
+  return 0
+}
+
+function sortArticlesByDate(articles) {
+  return articles.sort(datesComparison)
+}
+
+function App({ articles }) {
+  const [sortedArticles, setSortedArticles] = useState(
+    sortArticlesByUpvotes(articles)
+  )
+  const handleMostUpvoted = () =>
+    setSortedArticles([...sortArticlesByUpvotes(sortedArticles)])
+
+  const handleMostRecent = () => {
+    setSortedArticles([...sortArticlesByDate(sortedArticles)])
+  }
+
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-center justify-content-center my-20 navigation">
+        <label className="form-hint mb-0 text-uppercase font-weight-light">
+          Sort By
+        </label>
+        <button
+          data-testid="most-upvoted-link"
+          className="small"
+          onClick={() => handleMostUpvoted()}
+        >
+          Most Upvoted
+        </button>
+        <button
+          data-testid="most-recent-link"
+          className="small"
+          onClick={() => handleMostRecent()}
+        >
+          Most Recent
+        </button>
+      </div>
+      <Articles articles={sortedArticles} />
+    </div>
+  )
+}
+
+export default App
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React from 'react'
 
-function Articles() {
-
-    return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-
+function Article({ title, upvotes, date }) {
+  return (
+    <tr data-testid="article" key="article-index">
+      <td data-testid="article-title">{title}</td>
+      <td data-testid="article-upvotes">{upvotes}</td>
+      <td data-testid="article-date">{date}</td>
+    </tr>
+  )
 }
 
-export default Articles;
+function Articles({ articles }) {
+  return (
+    <div className="card w-50 mx-auto">
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles.map((article) => (
+            <Article
+              key={article.title}
+              title={article.title}
+              upvotes={article.upvotes}
+              date={article.date}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Articles
